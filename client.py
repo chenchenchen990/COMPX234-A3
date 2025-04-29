9786cb7b5a0de6226382d436c61d1449fcf6aa9e
chenchenchen990 <2995495734@qq.com>
2025-04-29 23:49:08 +0800

    Define create_message function for protocol formatting

--- client.py
+++ client.py
@@ -1,3 +1,9 @@
 import socket
 import sys
 import os
+
+def create_message(command, key, value=None):
+    """
+    Create a properly formatted protocol message to send to the server.
+    The message starts with a 3-digit length prefix (including itself).
+    """
